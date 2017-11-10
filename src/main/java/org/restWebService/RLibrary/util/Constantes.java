8bf9f0856dcd649a75afb4692471e9f847b4a083
manromero <miguelangelnunezromero@gmail.com>
2017-11-10 10:02:58 +0100

    allowed origins cors

--- src/main/java/org/restWebService/RLibrary/util/Constantes.java
+++ src/main/java/org/restWebService/RLibrary/util/Constantes.java
@@ -2,6 +2,8 @@ package org.restWebService.RLibrary.util;
 
 public class Constantes {
 	
+	public static final String URL_CLIENT = "https://rlibraryfront.herokuapp.com/";
+	
 	public static final String S_UPDATE = "update";
 	public static final String S_TRUE = "true";
 	public static final String S_FALSE = "true";
